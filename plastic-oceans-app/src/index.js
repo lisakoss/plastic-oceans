d8fbdd558f3b7650ddd942fd45d245f0ff50a7de
Lisa Koss <lisakoss@uw.edu>
2018-05-10 16:51:41 -0700

    Initialize Firebase

--- plastic-oceans-app/src/index.js
+++ plastic-oceans-app/src/index.js
@@ -4,5 +4,17 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyARk0V2zLTbb5TtSbKEiHx7g-yI6Cf2i80",
+  authDomain: "plastic-oceans.firebaseapp.com",
+  databaseURL: "https://plastic-oceans.firebaseio.com",
+  projectId: "plastic-oceans",
+  storageBucket: "plastic-oceans.appspot.com",
+  messagingSenderId: "244318961535"
+};
+firebase.initializeApp(config);
+
+// Render the application view
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
