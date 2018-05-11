446d3c0a4949182565eb3083f5cfd4ced0eab95c
Alexis Koss <alexis30@uw.edu>
2018-05-10 17:16:29 -0700

    add initial opening design

--- plastic-oceans-app/src/Opening.js
+++ plastic-oceans-app/src/Opening.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import './index.css';
 
+import { Link } from 'react-router-dom';
+import { Button } from 'reactstrap';
+
 export default class Opening extends React.Component {
   constructor(props) {
     super(props);
@@ -12,8 +15,12 @@ export default class Opening extends React.Component {
   render() {
 
     return (
-      <div>
-        <p>This is the opening screen. Will have sign up + sign in buttons.</p>
+      <div role="main" className="welcome-message">
+        <h1>Welcome to Plastic Oceans!</h1>
+          <div className="welcome-buttons">
+            <Link to="/signup"><Button color="primary" size="lg">Sign Up</Button></Link>{' '}
+            <Link to="/signin"><Button color="primary" size="lg">Sign In</Button></Link>
+          </div>
       </div>
     )
   }
