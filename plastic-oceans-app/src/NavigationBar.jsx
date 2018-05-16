cee12b09583ef8a601ad15dd69ddf431ce56ef56
Jane <janequichocho97@gmail.com>
2018-05-15 17:17:20 -0700

    Added click functionality

--- plastic-oceans-app/src/NavigationBar.jsx
+++ plastic-oceans-app/src/NavigationBar.jsx
@@ -3,10 +3,10 @@ import React, { Component } from "react";
 class NavigationBar extends Component {
     render() {
         return(
-            <div id="nav-bar">
-                <button className="nav-button" id="Discover" onClick={(e) => this.handleButtonClick(e)}><img src="https://image.flaticon.com/icons/png/512/44/44386.png" alt="earth icon" /><br />Discover</button>
-                <button className="nav-button" id="Footprint" onClick={(e) => this.handleButtonClick(e)}><img src="https://upload.wikimedia.org/wikipedia/commons/3/39/Gnomelogo-footprint.svg" alt="footprint icon" /><br />Footprint</button>
-                <button className="nav-button" id="Quizzes" onClick={(e) => this.handleButtonClick(e)}><img src="https://image.flaticon.com/icons/png/512/36/36601.png" alt="question mark icon" /><br />Quizzes</button>
+            <div ref="navBar" id="nav-bar">
+                <button ref="discoverButton" className="nav-button selected" id="Discover" onClick={(e) => this.handleButtonClick(e)}><img src="https://image.flaticon.com/icons/png/512/44/44386.png" alt="earth icon" /><br />Discover</button>
+                <button ref="footprintButton" className="nav-button" id="Footprint" onClick={(e) => this.handleButtonClick(e)}><img src="https://upload.wikimedia.org/wikipedia/commons/3/39/Gnomelogo-footprint.svg" alt="footprint icon" /><br />Footprint</button>
+                <button ref="quizzesButton" className="nav-button" id="Quizzes" onClick={(e) => this.handleButtonClick(e)}><img src="https://image.flaticon.com/icons/png/512/36/36601.png" alt="question mark icon" /><br />Quizzes</button>
             </div>
         );
     }
@@ -14,6 +14,11 @@ class NavigationBar extends Component {
     handleButtonClick(e) {
         var screenID = e.target.id;
         this.props.changeScreen(screenID);
+        var navButtons = this.refs.navBar.childNodes;
+        for (var i = 0; i < navButtons.length; i++) {
+            navButtons[i].classList.remove("selected");
+        }
+        e.target.classList.add("selected");
     }
 }
 
