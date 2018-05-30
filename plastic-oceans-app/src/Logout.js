b9518fe3fa4ab6fbe9ca8dc2f491dd9c496393ce
Alexis Koss <alexis30@uw.edu>
2018-05-29 17:22:20 -0700

    fix styles on sign in/up

--- plastic-oceans-app/src/Logout.js
+++ plastic-oceans-app/src/Logout.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './index.css';
 import firebase from 'firebase';
+import  { Redirect } from 'react-router-dom'
 
 import { Button } from 'reactstrap'
 
@@ -14,15 +15,27 @@ export default class Logout extends React.Component {
     this.signOutUser = this.signOutUser.bind(this);
   }
 
+  	//Lifecycle callback executed when the component appears on the screen.
+	componentDidMount() {
+		// Add a listener and callback for authentication events 
+		this.unregister = firebase.auth().onAuthStateChanged(user => {
+			if(user) {
+				this.setState({userId:user.uid}); //grabs user id
+			} else { //redirects to home page once logged out
+				this.setState({userId: null}); //null out the saved state
+			}
+		});
+  }
+
+	//when component will be removed
+  componentWillUnmount() {
+    if(this.unregister){ //if have a function to unregister with
+      this.unregister(); //call that function!
+    }
+}
+
   signOutUser() {
     firebase.auth().signOut();
-    this.unregister = firebase.auth().onAuthStateChanged(user => {
-      if (user) {
-        this.setState({ userId: user.uid }); // grabs user id
-      } else { // redirects to home page once logged out
-        this.setState({ userId: null }); // null out the saved state
-      }
-    });
   }
 
   render() {
--- plastic-oceans-app/src/Opening.js
+++ plastic-oceans-app/src/Opening.js
@@ -9,24 +9,52 @@ import Logout from './Logout';
 export default class Opening extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {}
+    this.state = {
+      userId: null,
+    }
+  }
+
+  /* Lifecycle callback:
+executed when the component appears on the screen. */
+  componentDidMount() {
+    /* Add a listener and callback for authentication events */
+    this.unregister = firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        this.setState({ userId: user.uid });
+      } else { // redircts user to login page if not logged in
+        this.setState({ userId: null }); //null out the saved state
+      }
+    })
+  }
+
+  /* Unregister listerns. */
+  componentWillUnmount() {
+    if (this.unregister) {
+      this.unregister();
+    }
   }
 
   render() {
-    return (
-      <div>
-      <div role="main" id="home" className="tinted">
-        <div className="welcome-message container-welcome">
-          <h1>Plastic Oceans</h1>
-          <h2>Help reduce your plastic footprint.</h2>
-          <div className="welcome-buttons">
-            <Link to="/signup"><Button outline color="primary" size="lg">Sign Up</Button></Link>{' '}
-            <Link to="/signin"><Button color="primary" size="lg" className="sign-in-button">Sign In</Button></Link>
-            <Logout/>
+    let logoutBtn = null;
+
+    if (this.state.userId !== null) {
+      logoutBtn = (<Logout />);
+    }
+
+      return (
+        <div>
+          <div role="main" id="home" className="tinted">
+            <div className="welcome-message container-welcome">
+              <h1>Plastic Oceans</h1>
+              <h2>Help reduce your plastic footprint.</h2>
+              <div className="welcome-buttons">
+                <Link to="/signup"><Button outline color="primary" size="lg">Sign Up</Button></Link>{' '}
+                <Link to="/signin"><Button color="primary" size="lg" className="sign-in-button">Sign In</Button></Link>
+                {logoutBtn}
+              </div>
+            </div>
           </div>
-          </div>
-          </div>
-      </div>
-    )
+        </div>
+      )
   }
 }
